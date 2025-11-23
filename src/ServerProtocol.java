46dc49ed2e217dab17f890fd89500f4793ebcde8
Viktor Nikander <vikvil91@gmail.com>
2025-11-23 09:07:25 +0100

    add ServerProtocol

---
+++ src/ServerProtocol.java
@@ -0,0 +1,24 @@
+public class ServerProtocol {
+    private final int INITIAL = 0;
+    private final int SUBJECT = 1;
+    private final int QUESTION = 2;
+
+    private int STATE = INITIAL;
+
+    QuestionBank qb = new QuestionBank();
+
+    public Object gameLoop(){
+        if (STATE == INITIAL){
+            STATE = SUBJECT;
+            return "CONNECTED";
+        } else if (STATE == SUBJECT) {
+            STATE = QUESTION;
+            return null;
+        } else if (STATE == QUESTION) {
+            STATE = SUBJECT;
+            return null;
+        } else {
+            return null;
+        }
+    }
+}
