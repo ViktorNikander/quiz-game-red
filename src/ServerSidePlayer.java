9046b5e254a75e8edec0ec715f69448b86a1c7ed
Viktor Nikander <vikvil91@gmail.com>
2025-11-24 13:53:31 +0100

    add send, receive

--- src/ServerSidePlayer.java
+++ src/ServerSidePlayer.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -12,8 +13,36 @@ public class ServerSidePlayer {
     public ServerSidePlayer(Socket socket, String player) {
         this.socket = socket;
         this.player = player;
+        try{
+            output = new ObjectOutputStream(socket.getOutputStream());
+            input = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Player died");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void send(Object outgoing){
+        try {
+            output.writeObject(outgoing);
+        } catch (IOException e) {
+            System.out.println();
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 
+    public Object receive(){
+        try {
+            return input.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     public void setOpponent(ServerSidePlayer opponent) {
