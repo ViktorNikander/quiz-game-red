cc86cde9c97c3cc715e391c2b5d51279c1051d0e
Viktor Nikander <vikvil91@gmail.com>
2025-11-24 12:51:34 +0100

    feature properties, TODO implement values

--- src/ServerProtocol.java
+++ src/ServerProtocol.java
@@ -1,11 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collections;
+import java.util.Properties;
 
 public class ServerProtocol {
     private final int INITIAL = 0;
     private final int SUBJECT = 1;
     private final int QUESTION = 2;
+    private int amountOfQuestions;
+    private int amountOfRounds;
 
     private int STATE = INITIAL;
+
+    public ServerProtocol(){
+        Properties p = new Properties();
+        try{
+            p.load(new FileInputStream("src/GameSettings.properties"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        amountOfQuestions = Integer.parseInt(p.getProperty("questionsPerRound", "2"));
+        amountOfRounds = Integer.parseInt(p.getProperty("roundsPerGame", "2"));
+        //TODO implement values in protocol
+    }
 
     QuestionBank qb = new QuestionBank();
 
