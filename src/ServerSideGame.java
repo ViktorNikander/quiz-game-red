c21052db4eda2e93372103b0a08b4d029a78ddf6
Viktor Nikander <vikvil91@gmail.com>
2025-11-24 14:58:08 +0100

    feature game settings via properties file

--- src/ServerSideGame.java
+++ src/ServerSideGame.java
@@ -1,7 +1,15 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ServerSideGame extends Thread{
     private ServerSidePlayer firstPlayer;
     private ServerSidePlayer secondPlayer;
     private ServerSidePlayer currentPlayer;
+    private QuestionBank questionBank = new QuestionBank();
+    private int nrOfQuestions;
+    private int nrOfRounds;
 
     public ServerSideGame(ServerSidePlayer firstPlayer, ServerSidePlayer secondPlayer) {
         this.firstPlayer = firstPlayer;
@@ -9,6 +17,19 @@ public class ServerSideGame extends Thread{
         this.currentPlayer = firstPlayer;
         this.firstPlayer.setOpponent(secondPlayer);
         this.secondPlayer.setOpponent(firstPlayer);
+
+        Properties p = new Properties();
+
+        try{
+            p.load(new FileInputStream("src/GameSettings.properties"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.nrOfQuestions = Integer.parseInt(p.getProperty("questionsPerRound", "2"));
+        this.nrOfRounds = Integer.parseInt(p.getProperty("roundsPerGame", "2"));
     }
 
     @Override
